46ed0bf894c48ba129ac25401ecfeee53e3b66d5
Dev deRev <devderev@free.fr>
2022-02-28 15:02:51 +0100

    first commit

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,32 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(name, health, strength)
+    this.name=name
+    this.health=health
+    this.strength=strength
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+    return this.health>0 ? `${this.name} has received ${damage} points of damage`:`${this.name} has died in act of combat`; 
+  }
+
+  battleCry(){}
+}
 
 // Saxon
 class Saxon {}
